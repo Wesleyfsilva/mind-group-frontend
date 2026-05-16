9a252039e0fe0ce59714999dd8ad56f6d4bc711d
Wesley Ferreira <wesleyfsilva97@gmail.com>
2026-05-16 03:07:34 -0300

    refactor: remove comentários internos e finaliza limpeza de código

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001', // Endereço do seu backend Node
+  baseURL: 'http://localhost:3001', 
 });
 
-// Esse código anexa o token de login automaticamente em cada requisição
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('@MindBlog:token');
   if (token) {
